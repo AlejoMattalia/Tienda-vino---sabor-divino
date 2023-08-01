ce07aa1882f1edb50a64c8e272fce2f235bc106b
AlejoMattalia <aleacade.mattalia@gmail.com>
2023-08-01 01:40:50 -0300

    solcuion main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import App from "./App.jsx";
-import { ConfigContextProvider } from "./context/ConfigContext";
+import { ConfigContextProvider } from "./context/ConfigContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
